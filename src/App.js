df6bacb1fbcdab72f5bbed7482fb8cf9274c86c3
Sdsdsde2 <imspammails31@gmail.com>
2021-07-04 12:21:29 -0500

    added react routs and made registration route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import {BrowserRouter, Switch, Route, Link} from "react-router-dom";
+import axios from "axios";
+import Register from './components/register'
 
-function App() {
+export default class App extends Component{
+  constructor() {
+    super();
+
+    this.state = {
+      loggedInStatus: "NOT_LOGGED_IN",
+      user: {}
+    }
+
+    this.handleSuccessfulAuth = this.handleSuccessfulAuth.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  handleSuccessfulAuth(data) {
+    this.props.handleLogin(data);
+    this.props.history.push("/user");
+  }
+
+  checkLoginStatus() {
+    axios.get("http://localhost:3000/logged_in", { withCredentials: true })
+    .then(resp => {
+      if (resp.data.logged_in && this.state.loggedInStatus === "NOT_LOGGED_IN")
+        this.setState({
+          loggedInStatus: "LOGGED_IN",
+          user: resp.data.user
+        })
+      else if (!resp.data.logged_in && this.state.loggedInStatus === "LOGGED_IN") {
+        this.setState({
+          loggedInStatus: "NOT_LOGGED_IN",
+          user: {}
+        })
+      }
+    })
+    .catch(error => console.log("Login error", error))
+  }
+
+  componentDidMount() {
+    this.checkLoginStatus();
+  }
+
+  handleLogin(data) {
+    this.setState({
+      loggedInStatus: "LOGGED_IN",
+      user: data.user
+    })
+  }
+
+  handleLogout() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN",
+      user: {}
+    });
+  }
+
+  render() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <BrowserRouter>
+        <div className="headerLink">
+          <Link to="/register" className="linkStyle">Register</Link>
+        </div>
+        <Route path="/register" render={props => (
+          <Register {... props} handleLogin={this.handleLogin} handleLogout={this.handleLogout} loggedInStatus={this.state.loggedInStatus} />
+        )} />
+      </BrowserRouter>
       </header>
     </div>
   );
+  }
 }
-
-export default App;
---
+++ src/components/auth/registration.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+
+export default class Registration extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            first_name: "",
+            last_name: "",
+            user_name: "",
+            password: "",
+            password_confirmation: "",
+            registerErrors: ""
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        axios.post("http://localhost:3000/registrations", {
+            first_name: this.state.first_name,
+            last_name: this.state.last_name,
+            user_name: this.state.user_name,
+            password: this.state.password,
+            password_confirmation: this.state.password_confirmation
+        },
+        {withCredentials: true})
+        .then(resp => {
+            if (resp.data.status === 'created') 
+                this.props.handleSuccessfulAuth(resp.data)
+            })
+        .catch(error => console.log("Registration Errors:", error))
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input 
+                        type="first_name" 
+                        name="first_name" 
+                        placeholder="First Name" 
+                        value={this.state.first_name} 
+                        onChange={this.handleChange} 
+                        required 
+                    />
+                    <input 
+                        type="last_name" 
+                        name="last_name" 
+                        placeholder="Last Name" 
+                        value={this.state.last_name} 
+                        onChange={this.handleChange} 
+                        required 
+                    />
+                    <input 
+                        type="user_name" 
+                        name="user_name" 
+                        placeholder="Username" 
+                        value={this.state.user_name} 
+                        onChange={this.handleChange} 
+                        required 
+                    />
+                    <input 
+                        type="password" 
+                        name="password" 
+                        placeholder="Password" 
+                        value={this.state.password} 
+                        onChange={this.handleChange} 
+                        required 
+                    />
+                    <input 
+                        type="password" 
+                        name="password_confirmation" 
+                        placeholder="Confirm Password" 
+                        value={this.state.password_confirmation} 
+                        onChange={this.handleChange} 
+                        required 
+                    />
+                    <button type="submit">Register</button>
+                </form>
+            </div>
+        )
+    }
+}
